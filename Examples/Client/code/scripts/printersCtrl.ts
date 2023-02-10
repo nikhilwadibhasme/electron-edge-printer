ae7195cffaa75b0ca1b1b22836f243290f26a554
nikhilwadibhasme <nikhilbwadibhasme@gmail.com>
2023-02-10 10:27:47 +0530

    Updated code

--- Examples/Client/code/scripts/printersCtrl.ts
+++ Examples/Client/code/scripts/printersCtrl.ts
@@ -55,14 +55,14 @@ module app {
 
 
           var edgeprinter = require('electron-edge-printer');
-          edgeprinter.getSupportedPageSizesforPrinter('\\\\CSPUDCA01\\EPSON L550 Series', function (error, result) {
+          edgeprinter.getSupportedPageSizesforPrinter('<PrinterPath>', function (error, result) {
               if (error) throw error;
               var lstpageSizes = result;
               console.log('available page sizes:-');
               console.log(lstpageSizes);
           });
 
-          edgeprinter.getSupportedPageOrientationsforPrinter('Canon iR3235/iR3245 PCL6', function (error, result) {
+          edgeprinter.getSupportedPageOrientationsforPrinter('<PrinterPath>', function (error, result) {
               if (error) throw error;
               var lstpageOrientation = result;
               console.log('available page Orientation:-');
@@ -71,7 +71,7 @@ module app {
 
 
           var PageSizeInfo = {
-              printerName: 'Canon iR3235/iR3245 PCL6',
+              printerName: '<PrinterPath>',
               pageSizeName: 'ISOA3'
           };
           edgeprinter.setPageSizeforPrinter(PageSizeInfo, function (error, result) {
@@ -80,7 +80,7 @@ module app {
           });
 
           var PageOrientationInfo = {
-              printerName: 'Canon iR3235/iR3245 PCL6',
+              printerName: '<PrinterPath>',
               pageOrientation: 'Portrait'
           };
           edgeprinter.setPageOrientationforPrinter(PageOrientationInfo, function (error, result) {
@@ -89,14 +89,14 @@ module app {
           });
 
         
-          edgeprinter.getAvailableTraysforPrinter('Canon iR3235/iR3245 PCL6', function (error, result) {
+          edgeprinter.getAvailableTraysforPrinter('<PrinterPath>', function (error, result) {
               if (error) throw error;
               console.log('Available Trays for Printer:-');
               console.log(result);
           });
 
           var PageTrayInfo = {
-              printerName: 'Canon iR3235/iR3245 PCL6',
+              printerName: '<PrinterPath>',
               TrayName: 'ns0000:ManualFeed'
           };
 
@@ -106,14 +106,14 @@ module app {
           });
 
         
-          edgeprinter.getSupportedOutPutColorsforPrinter('\\\\cspudca01\\PU_HP_LaserJet_4250_PCL6', function (error, result) {
+          edgeprinter.getSupportedOutPutColorsforPrinter('<PrinterPath>', function (error, result) {
               if (error) throw error;
               console.log('Available Output Colors for Printer:-');
               console.log(result);
           });
 
           var OutputColorInfo = {
-              printerName: '\\\\cspudca01\\PU_HP_LaserJet_4250_PCL6',
+              printerName: '<PrinterPath>',
               OutPutColor: 'Grayscale'
           };
 
